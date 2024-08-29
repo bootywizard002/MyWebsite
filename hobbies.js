fc3c6028ef95c39a76027d7e64907ffa57bd9991
bootywizard002 <bootywizard002@gmail.com>
2024-08-29 09:36:12 +0900

    just more edits

--- hobbies.js
+++ hobbies.js
@@ -20,9 +20,30 @@ span.onclick = function () {
 };
 
 window.onclick = function (event) {
-  console.log('window is clicked');
+  //   console.log('window is clicked');
   if (event.target == modal) {
     console.log('hit on modal');
     modal.style.display = 'none';
   }
 };
+
+function scrollToTop(event) {
+  event.preventDefault(); // Prevent the default anchor behavior
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth', // Smooth scrolling
+  });
+}
+//navbar selectors
+const navBarIcon = document.querySelector('.navicon-container');
+const navBarIconIcon = document.querySelector('.navbar-icon');
+const navigationBar = document.querySelector('.navbar');
+navBarIcon.addEventListener('click', navbarPressed);
+function navbarPressed() {
+  console.log('hello');
+  navigationBar.classList.toggle('active'); // Toggle visibility class
+
+  // Toggle icon classes
+  navBarIconIcon.classList.toggle('fa-xmark');
+  navBarIconIcon.classList.toggle('fa-bars');
+}
